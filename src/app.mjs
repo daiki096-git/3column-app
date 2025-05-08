43220b21e44a4601d1a35b20db08c50abbdf2051
daiki <i.daikinyonyo@gmail.com>
2025-05-08 19:33:29 +0900

    directory modified

--- src/app.mjs
+++ src/app.mjs
@@ -4,10 +4,17 @@ import cors from "cors";
 import logger from '../config/logger.mjs';
 import routes from "./routes/index.mjs";
 import session_middle from '../config/session.mjs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
-const app = express();
 
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(session_middle);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

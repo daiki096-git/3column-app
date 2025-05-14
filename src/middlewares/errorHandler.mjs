5ea8f35c3ae0b0ee75781c1d2613564384e69cc2
daiki <i.daikinyonyo@gmail.com>
2025-05-14 15:06:01 +0900

    エラーハンドリングの表示方法を修正

--- src/middlewares/errorHandler.mjs
+++ src/middlewares/errorHandler.mjs
@@ -4,7 +4,7 @@ export const requestErrorHandler = (controller) => {
     return async (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
+          return res.status(400).json({ message:errors.array()[0].msg,errors: errors.array() });
         }    
         try {
             await controller(req, res)
